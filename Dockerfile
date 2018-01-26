c5d0f230050b334b7578795547c49d7298ae4e4d
Barry Rowlingson <b.rowlingson@lancs.ac.uk>
2018-01-26 16:40:39 +0000

    run model fitting from data

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,5 @@ WORKDIR /app
 
 ADD . /app
 
-RUN git clone https://github.com/barryrowlingson/phlabr.git
-
 CMD ["./run.sh"]
 
--- time.r
+++ time.r
@@ -4,3 +4,15 @@
 
 library(devtools)
 load_all("./phlabr")
+
+message("Command args are...")
+print(argv)
+
+data=argv[1]
+
+message("Reading from ",data)
+
+m = fit_time_file(data)
+
+print(summary(m))
+
