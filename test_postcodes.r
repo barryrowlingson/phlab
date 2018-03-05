cb212639af60dfb6c0bdb19b750973a8d3722f8d
Barry Rowlingson <b.rowlingson@lancs.ac.uk>
2018-03-05 15:01:01 +0000

    test unique postcodes

--- test_postcodes.r
+++ test_postcodes.r
@@ -2,7 +2,10 @@
 
 # given a CSV of postcodes, see how many exist in a lookup table.
 
-library(devtools, warn.conflicts=FALSE, quietly=TRIE)
+
+library(RSQLite, warn.conflicts=FALSE, quietly=TRUE)
+library(utils, warn.conflicts=FALSE, quietly=TRUE)
+library(devtools, warn.conflicts=FALSE, quietly=TRUE)
 load_all("./phlabr", quiet=TRUE)
 
 pc_db = argv[1]
@@ -10,18 +13,20 @@ pc_csv = argv[2]
 pc_column = argv[3]
 
 testcodes = read_postcodes_file(pc_csv)
-testcodes = data.frame(postcode = unique(testcodes[[pc_column]]))
+message("Testing ",nrow(testcodes)," records")
+
+unique_testcodes = data.frame(postcode = unique(testcodes[[pc_column]]))
 
 pc_db = get_postcodes_db(pc_db, "postcodes","postcode")
 
-testcodes$OK = test_postcodes(testcodes[[pc_column]], pc_db$postcode)
+unique_testcodes$OK = test_postcodes(unique_testcodes[[pc_column]], pc_db$postcode)
 
-total <- nrow(testcodes)
-found <- sum(testcodes$OK)
+total <- nrow(unique_testcodes)
+found <- sum(unique_testcodes$OK)
 
 message("Postcode database size: ",nrow(pc_db))
 
-message("Looked up ",total," postcodes, matched ",found)
+message("Looked up ",total," unique postcodes, matched ",found)
 
 
 
