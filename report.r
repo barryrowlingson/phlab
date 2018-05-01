61400b4cf02c1c3b16e27f7897a50ba8769af83a
Barry Rowlingson <b.rowlingson@lancs.ac.uk>
2018-05-01 12:31:07 +0100

    reports

--- report.r
+++ report.r
@@ -9,9 +9,14 @@ report = argv[1]
 report_file = file.path("./reports",paste0(report,".Rmd"))
 
 daydir = format(Sys.time(),"%F")
-output_dir = normalizePath(file.path("./outputs/",daydir))
-dir.create(output_dir, showWarnings=FALSE, recursive=TRUE)
-output_file = tempfile(pattern=paste0(report,"-"), tmpdir=output_dir, fileext=".pdf")
+output_dir = file.path("./outputs/",daydir)
+dir.create(output_dir, recursive=TRUE)
+output_dir = normalizePath(output_dir)
+message("Output directory is ",output_dir)
+
+output_file =  tempfile(pattern=paste0(report,"-"), tmpdir=output_dir, fileext=".pdf")
+
+message("Output file is ",output_file)
 
 render(report_file, output_file=output_file)
 
