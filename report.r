c512700be39b8e6a15b893da89c150faf3f48348
Barry Rowlingson <b.rowlingson@lancs.ac.uk>
2018-05-02 12:03:56 +0100

    options and multiple outputs

--- report.r
+++ report.r
@@ -9,14 +9,26 @@ report = argv[1]
 report_file = file.path("./reports",paste0(report,".Rmd"))
 
 daydir = format(Sys.time(),"%F")
-output_dir = file.path("./outputs/",daydir)
+output_dir = file.path("./outputs/",daydir, tempfile(pattern=paste0(report,"-"),tmpdir=""))
 dir.create(output_dir, recursive=TRUE)
 output_dir = normalizePath(output_dir)
 message("Output directory is ",output_dir)
 
-output_file =  tempfile(pattern=paste0(report,"-"), tmpdir=output_dir, fileext=".pdf")
+# remove the markdown doc name
+argv=argv[-1]
 
-message("Output file is ",output_file)
+# args with spaces get broken unless protected, and this isn't called..
+qs = function (x){
+    ifelse(stringr::str_detect(x, "\\s"), shQuote(x), x)
+}
+argv=qs(argv)
 
-render(report_file, output_file=output_file)
+# stick argv in the environment so it can be found in the document
+e = new.env()
+assign("argv",argv,e)
+
+# render the document.
+render(report_file, output_dir=output_dir, output_format="all", envir=e)
+
+yaml = yaml_front_matter(report_file)
 
